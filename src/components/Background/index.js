fbbd8a7a8c33675742f60564966f87e2196e46a9
Stenia Felix <steniafelix@gmail.com>
2019-10-21 06:35:38 -0300

    Adding components Button, Input and background

---
+++ src/components/Background/index.js
@@ -0,0 +1,8 @@
+import LinearGradiente from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+
+export default styled(LinearGradiente).attrs({
+  colors: ['rgb(50, 127, 290)', '#00BFFF'],
+})`
+  flex: 1;
+`;
